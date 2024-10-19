cdc2fa13d24ad0948c089dc65778bda3e2e3f9e5
V-Kozintsev <V-Kozintsev@users.noreply.github.com>
2024-10-19 16:27:49 +0000

    deploy: 67cb17e0756ec6652903fe860d90c710d2a471ef

---
+++ main.main.ts
@@ -0,0 +1,44 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+
+;// ./src/addform.ts
+function createForm() {
+  // Создаем input элемент
+  const input = document.createElement('input');
+  input.type = 'text'; // Устанавливаем тип текстового поля
+
+  // Создаем кнопку
+  const button = document.createElement('button');
+  button.innerText = 'Отправить'; // Устанавливаем текст на кнопке
+
+  // Создаем элемент для отображения результата
+  const output = document.createElement('div');
+
+  // Добавляем обработчик события на кнопку
+  button.addEventListener('click', () => {
+    const {
+      value
+    } = input;
+    output.innerText = `Вы ввели: ${value}`;
+    input.value = ''; // Очищаем input после отправки
+  });
+
+  // Обработчик события для нажатия клавиши Enter
+  input.addEventListener('keypress', event => {
+    if (event.key === 'Enter') {
+      button.click(); // Вызываем событие клика на кнопке
+    }
+  });
+
+  // Добавляем элементы на страницу
+  document.body.appendChild(input);
+  document.body.appendChild(button);
+  document.body.appendChild(output);
+}
+;// ./src/index.ts
+
+window.onload = function () {
+  createForm(); // Создаем форму при загрузке страницы
+};
+/******/ })()
+;
